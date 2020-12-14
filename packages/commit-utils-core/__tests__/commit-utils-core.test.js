80698d4482cda23906327a47de6cca56d7ccf8c8
Jun Cho <36058447+ju-nc@users.noreply.github.com>
2020-12-14 14:47:31 -0500

    test: Add commit-utils-core test
    test: Add commit-utils-core test

--- packages/commit-utils-core/__tests__/commit-utils-core.test.js
+++ packages/commit-utils-core/__tests__/commit-utils-core.test.js
@@ -1,5 +1,30 @@
 'use strict'
+const constants = require('../src/constants')
+const { COMMIT_TYPES } = constants
 
 describe('commit-utils-core', () => {
-    it('needs tests', () => {})
+    it.each`
+        commitType    | appearsInChangelog
+        ${'wip'}      | ${false}
+        ${'feat'}     | ${true}
+        ${'fix'}      | ${true}
+        ${'cr'}       | ${false}
+        ${'style'}    | ${false}
+        ${'refactor'} | ${false}
+        ${'perf'}     | ${true}
+        ${'docs'}     | ${false}
+        ${'test'}     | ${false}
+        ${'revert'}   | ${true}
+        ${'build'}    | ${false}
+        ${'ci'}       | ${false}
+        ${'chore'}    | ${false}
+    `(
+        'commit type $commitType has the correct appearsInChangelog value of $appearsInChangelog',
+        ({ commitType, appearsInChangelog }) => {
+            expect(COMMIT_TYPES[commitType].appearsInChangelog).toEqual(
+                appearsInChangelog,
+            )
+            expect(COMMIT_TYPES[commitType].prefix).toEqual(commitType)
+        },
+    )
 })
