3e2c133119f37a4f343b36e833dab373ee71bb3a
Jun Cho <36058447+ju-nc@users.noreply.github.com>
2020-12-15 18:15:16 -0500

    cr: remove snapshot test

--- packages/commitlint-config/__tests__/commitlint-config.test.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-const { rules } = require('../src')
-
-describe('commitlint-config', () => {
-    it('config matches snapshot', () => {
-        expect(rules).toMatchSnapshot()
-    })
-})
