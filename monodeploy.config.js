fa2b00b78c85affb479c10c45bfc6f528ee83e11
Noah Negin-Ulster <noah.negin-ulster@tophatmonocle.com>
2022-02-10 11:53:43 -0500

    refactor: use monodeploy to release

---
+++ monodeploy.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    git: {
+        push: true,
+    },
+    changesetIgnorePatterns: ['**/*.test.ts', '**/*.test.js'],
+    conventionalChangelogConfig: '@tophat/conventional-changelog-config',
+    autoCommit: true,
+    autoCommitMessage: 'chore: release commit-watch [skip ci]',
+    changelogFilename: 'CHANGELOG.md',
+    persistVersions: true,
+}
